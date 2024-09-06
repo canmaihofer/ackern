7660c4038494c6225a2ca37bfa9059dd37a26cfb
Can <s3camaih@gmail.com>
2024-09-06 18:35:32 +0200

    update

--- app/components/TimeTable.tsx
+++ app/components/TimeTable.tsx
@@ -3,9 +3,15 @@ export default function TimeTable() {
     <section className="py-20">
       <div className="container mx-auto">
         <h2 className="text-3xl font-bold text-center mb-12">Time Table</h2>
-        <h4 className="text-xl font-bold text-center mb-12">
-          Surprise Surprise
-        </h4>
+        <div className="w-full flex justify-center">
+          <div>
+            <img
+              src="/images/timetable.webp"
+              alt="Festival Location Map"
+              className="w-full h-auto max-w-sm rounded-lg shadow-lg"
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
